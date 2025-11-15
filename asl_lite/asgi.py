63b2e5c99567c4005d414aeffab3d2221f16e9e6
yukio031 <163792014+yukio031@users.noreply.github.com>
2025-11-15 15:28:13 +0800

    Refactor ASGI configuration for project structure

--- asl_lite/asgi.py
+++ asl_lite/asgi.py
@@ -1,26 +1,15 @@
-"""
-ASGI config for asl_lite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
-# aslsite/asgi.py
+# myproject/asgi.py
 import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-import translator.routing
+from myproject.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asl_lite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            translator.routing.websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
