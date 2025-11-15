a5834bd26b019364c4a5cb3c4aeb86f0c90e4914
yukio031 <163792014+yukio031@users.noreply.github.com>
2025-11-15 16:03:39 +0800

    Add import statement for os module

--- asl_lite/settings.py
+++ asl_lite/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
