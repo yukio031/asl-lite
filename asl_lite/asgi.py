efe6333e3d1f39afc6e49e75f7e2b4a0572026d3
yukio031 <163792014+yukio031@users.noreply.github.com>
2025-11-15 16:13:27 +0800

    Update asgi.py

--- asl_lite/asgi.py
+++ asl_lite/asgi.py
@@ -1,17 +1,17 @@
+# asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from translator import consumers
+from . import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asl_lite.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path('ws/translate/', consumers.ASLConsumer.as_asgi()),
-        ])
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
     ),
 })
