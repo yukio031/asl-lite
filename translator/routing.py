54c73833662dcb5f1128a6e8db9209cc55fb1f8d
yukio031 <163792014+yukio031@users.noreply.github.com>
2025-11-15 15:48:33 +0800

    Replace re_path with path for WebSocket routing

--- translator/routing.py
+++ translator/routing.py
@@ -1,7 +1,6 @@
-# myproject/routing.py
-from django.urls import re_path
-from translator import consumers
+from django.urls import path
+from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/translate/$', consumers.ASLConsumer.as_asgi()),
+    path('ws/translate/', consumers.ASLConsumer.as_asgi()),  # WebSocket route
 ]
